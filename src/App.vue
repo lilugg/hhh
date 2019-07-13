b88419cb306ea4b533d37dae3245ea6d384e5780
Pinklu! <1255193184@qq.com>
2019-07-13 09:01:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+     <header>
+       <div class="loge">
+        <p class="iconfont icon-angle-left"></p>
+        <p><span class="iconfont icon-fangdajing"></span>面试列表</p>
+        <p class="iconfont icon-gengduo"></p>
+       </div>
+      <div class="list">
+        <span v-for="(item,index) in dataJson" :class="{active:index==num}" @click="click(index)" :key="index">{{item.type}}</span>
+      </div>
+     </header>
+     <section>
+       <ul>
+           <p class="p" v-if="num==0" style=" text-align: center;  margin-top: 30px; color:#666; flex:1 background:#fff">{{msg}}</p>
+         <HelloWorld v-for="(item,index) in dataJson[num].list" :key="index" :option="item"/>
+        
+       </ul>
+     </section>
+     <footer></footer>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import dataJson from './data/data.json'
+export default {
+  name: 'App',
+  data(){
+    return{
+      dataJson,
+      num:1,
+      msg:""
+    }
+  },
+  methods:{
+click(index){
+  if(index==0){
+     this.msg="当前分类还没有面试！"
+  }
+this.num=index;
+}
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import"./conmons.scss";
+#app ,section{
+ width: 100%;
+ height: 100%;
+ overflow: hidden;
+ display: flex;
+ flex-direction: column;
+ background: #EEEEEE;
+}
+header{
+  width: 100%;
+  height: 80px;
+  padding: 0 5px;
+  background-color: #fff;
+  .loge{
+    width: 100%;
+    height: 40px;
+    display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+  }
+}
+.list{
+  display: flex;
+  height: 40px;
+  justify-content: space-between;
+  align-items: center;
+}
+.p{
+  width: 100%;
+height: 552px;
+overflow: hidden;
+background-color: #fff;
+}
+.list span{
+  padding-bottom:12px;
+}
+.list span.active{
+  border-bottom: 2px solid #4281BD;
+  color:#4281BD;
+
+
+}
+ul{
+  width: 100%;
+  flex:1;
+  overflow:auto; 
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+      <li>
+          <h2><p>{{option.title}}</p><span class="fang tip" >{{option.abandon}}</span></h2>
+          <p class="side">{{option.site}}</p>
+          <h4><p>面试时间：{{option.time}}{{option.hour}}</p><span class="ti">{{option.hint}}</span></h4>
+        </li>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props:["option"]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ li{
+  margin-top: 10px;
+  height: 150px;
+  width: 100%;
+  background-color: #fff;
+ 
+  
+  h2{
+   display: flex;
+   justify-content: space-between;
+   padding: 8px;
+   p{
+     font-size: 20px;
+     font-weight: 900;
+   }
+  }
+}
+.fang{
+  height: 22px;
+  line-height: 22px;
+  text-align: center;
+  padding: 0 5px;
+  font-size: 12px;
+
+}
+.side{
+  color: #BEBEBE;
+margin-left: 8px;
+}
+h4{
+  height: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #707070;
+ margin: 8px;
+ font-size: 14px;
+}
+.ti{
+  font-size: 12px;
+  background-color: #eee;
+  padding: 5px;
+}
+.tip{
+  color:#E88C89;background:#FCF1F1
+}
+</style>
